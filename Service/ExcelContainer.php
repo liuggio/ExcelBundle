58ff8ab0fb1182f01cb09b456a0803cfaa32be48
liuggio <liuggio@gmail.com>
2011-11-24 23:07:07 +0100

    added setHeader in the response

--- Service/ExcelContainer.php
+++ Service/ExcelContainer.php
@@ -11,10 +11,10 @@ class ExcelContainer
     protected $stream_writer;
     protected $file_type;
     protected $response_class;
-
+    protected $response_extension;
     protected $writer; 
 
-    public function __construct($excelObj, $factory_class, $factory_method, $factory_write_method = 'save', $stream_writer, $file_type = 'Excel5', $response_class)
+    public function __construct($excelObj, $factory_class, $factory_method, $factory_write_method = 'save', $stream_writer, $file_type = 'Excel5', $response_class, $extension = 'xls')
     {
         $this->excelObj = $excelObj;
         $this->factory_class = $factory_class;
@@ -23,6 +23,7 @@ class ExcelContainer
         $this->stream_writer = $stream_writer;
         $this->file_type = $file_type;
         $this->response_class = $response_class;
+        $this->response_extension = $extension;
         $actory_class= $this->factory_class;
          
     }
@@ -48,12 +49,32 @@ class ExcelContainer
         return $this->stream_writer;
     }
 
-    /*
+    /**
     *  create the response with the file content
     */
     public function getResponse()
     {   
         $this->createStreamWriter();
         return new $this->response_class($this->stream_writer);
+    }
+    
+    /**
+    * @TODO
+    */
+    public function addHeader($response, $filename = null, $extension)
+    {
+         if (isset($this->response_extension)) {
+             
+         }   
+    }
+    
+    /**
+    * @TODO
+    */
+    public function getResponseWithHeader($filename)
+    {
+        $response = $this->getResponse();
+        $this->addHeader($response, $filename, $this->response_extension);
+        
     }
 }
