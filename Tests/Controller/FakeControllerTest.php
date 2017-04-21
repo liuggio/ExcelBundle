b03f1287cecbc5796b31f1d72a3603b64bc671cc
Lesnykh Ilia <Aliance@users.noreply.github.com>
2017-04-21 13:29:55 +0300

    Update FakeControllerTest.php

--- Tests/Controller/FakeControllerTest.php
+++ Tests/Controller/FakeControllerTest.php
@@ -71,4 +71,14 @@ class FakeControllerTest extends WebTestCase
 
         $this->assertFileExists($content, sprintf('file %s should exist', $content));
     }
+
+    /**
+     * @inheritdoc
+     */
+    protected static function getKernelClass()
+    {
+        require_once __DIR__.'/../app/AppKernel.php';
+
+        return 'Liuggio\ExcelBundle\Tests\app\AppKernel';
+    }
 }
